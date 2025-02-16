4cf73e0c11841a64f749e9d095f22ac5ac3df922
horse-technologies <horsetech34@gmail.com>
2025-02-16 12:40:59 +0000

    hot fix page breaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import Home from './pages/Home';
-import About from './pages/About';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
-import ContactUs from './pages/ContactUs';
+import ContactUsPage from './pages/ContactUs';
 import TeamPage from './pages/Team';
-import Partners from './pages/Partners';
+import PartnersPage from './pages/Partners';
 import WaitListPage from './pages/WaitList';
 
 const router = createBrowserRouter([
@@ -14,11 +14,11 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Home /> },
-      { path: '/about', element: <About /> },
-      { path: '/contact-us', element: <ContactUs /> },
-      { path: '/team', element: <Team /> },
-      { path: '/partners', element: <Partners /> },
+      { index: true, element: <HomePage /> },
+      { path: '/about', element: <AboutPage /> },
+      { path: '/contact-us', element: <ContactUsPage /> },
+      { path: '/team', element: <TeamPage /> },
+      { path: '/partners', element: <PartnersPage /> },
       { path: '/waitlist', element: <WaitListPage /> },
     ],
   },
